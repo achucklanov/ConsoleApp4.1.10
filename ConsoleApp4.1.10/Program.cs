27ea51fdccf03b535143dcb611c1fb03aeb59f11
AndreyChuck <a.chucklanov@yandex.ru>
2023-03-08 20:40:55 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp4.1.10/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp4._1._10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Inv = true;
+            var Res = !Inv;
+            
+            Console.WriteLine(Res);
+            Console.ReadKey();
+        }
+    }
+}
